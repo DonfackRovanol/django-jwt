f5aec7ad9161fe4a690139386db0993cf7e813b1
donfack sahague <114002833+DonfackRovanol@users.noreply.github.com>
2023-04-23 22:41:09 +0100

    Update and rename mysite/info.py to mon site/info.py

--- mon site/info.py
+++ mon site/info.py
@@ -4,5 +4,5 @@ from pickle import TRUE
 EMAIL_USE_TLS = TRUE
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'rovanoldonfack7@gmail.com'
-EMAIL_HOST_PASSWORD = 'hmguxentltyeksyd'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
